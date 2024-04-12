0cb04664994af4cb09187219aab7a891ca1b7ced
Eberpraw <toby@easy.poker>
2024-04-12 13:39:02 +0200

    Added iPad sizes

--- src/main/sample/App.java
+++ src/main/sample/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("sample.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 810);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello Hello!");
         stage.setScene(scene);
         stage.show();
     }
