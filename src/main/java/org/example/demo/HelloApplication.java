a1e24f9343de01d38a41459f480c2c42cf37fdfc
Cunosankey <146829520+Cunosankey@users.noreply.github.com>
2024-04-12 11:03:36 +0200

    yo

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -4,6 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.Parent;
+
 
 import java.io.IOException;
 
