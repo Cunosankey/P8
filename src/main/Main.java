e46d232e024137e5355c69a9ae026aa6f05642b7
Cunosankey <146829520+Cunosankey@users.noreply.github.com>
2024-04-12 11:24:28 +0200

    yo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package src.main.java.org.example.demo;
+package PACKAGE_NAME.src.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,10 +13,10 @@ import javafx.scene.Parent;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class bob extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(bob.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
