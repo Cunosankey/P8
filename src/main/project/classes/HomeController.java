1b68951103ff93b343f0e4dad0f4c172a85887f9
Rasmus Hvass Olesen <rasmush.olesen@gmail.com>
2024-04-15 12:01:38 +0200

    Lort

--- src/main/project/classes/HomeController.java
+++ src/main/project/classes/HomeController.java
@@ -2,13 +2,30 @@ package PACKAGE_NAME.src.main.project.classes;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HomeController {
     @FXML
     private Label helloLabel;
 
     @FXML
+    private TextField inputField;
+    private String name;
+
+    @FXML
+    public void initialize() {
+        name = inputField.getText();
+    }
+
+    @FXML
     public void sayHello() {
         helloLabel.setText("Hello, JavaFX!");
+    }
+
+
+    public void displayVariable() {
+        String myVariable = name;
+        inputField.setText(myVariable);
+        System.out.println(name);
     }
 }
