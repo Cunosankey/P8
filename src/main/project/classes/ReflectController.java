5537663bd9faf2c4183a1c458d7dbcfcb08af9ba
NadjaDH <Ndha20@student.aau.dk>
2024-05-01 09:35:57 +0200

    Update ReflectController.java

--- src/main/project/classes/ReflectController.java
+++ src/main/project/classes/ReflectController.java
@@ -83,28 +83,4 @@ public class ReflectController implements Initializable {
         Parent root = fxmlLoader.load(); // Load the FXML file into a Parent object
         scene.setRoot(root); // Set the scene root to the new FXML file
     }
-  
-    @FXML
-    private HBox circleContainer;
-
-    public StoryProgress storyProgress;
-
-    public void initialize() {
-        // Create the ProgressManager object
-        storyProgress = new StoryProgress(circleContainer, 4);
-        storyProgress.resetCircles();
-        storyProgress.createCircles();
-        storyProgress.fillCircle("Reflect");
-    }
-
-
-    public void done(ActionEvent event) throws IOException {
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        Scene scene = stage.getScene();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../scenes/Library.fxml"));
-        Parent root = fxmlLoader.load();
-        scene.setRoot(root);
-    }
-  
 }
