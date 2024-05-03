b1a78c982519d3320eb0c51b1e179f84b1c376ce
Eberpraw <toby@easy.poker>
2024-05-03 22:03:23 +0200

    Added functionality to stop audio when changing scene

--- src/main/project/classes/TaskController.java
+++ src/main/project/classes/TaskController.java
@@ -194,6 +194,7 @@ public class TaskController implements Initializable {
 
     // Method to go to the Library scene
     public void TaskToLibrary(ActionEvent event) throws IOException {
+        AudioController.stopAudio();
         Node node = (Node) event.getSource(); // Get the source of the event (the button)
         Stage stage = (Stage) node.getScene().getWindow(); // Get the stage from the button
         Scene scene = stage.getScene(); // Get the scene from the stage
@@ -212,6 +213,8 @@ public class TaskController implements Initializable {
         // Update image methods before going to Reflect scene to ensure the images are updated
         updateCharacterFacialExpression();
         updateCharacterGesture();
+
+        AudioController.stopAudio();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../scenes/Reflect.fxml"));
         // lambda function that creates a new instance of ReflectController
