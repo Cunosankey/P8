92aea1c04a0dfd9ab9984224d5953151ef4d22c3
Rasmus Hvass Olesen <rasmush.olesen@gmail.com>
2024-05-06 12:22:22 +0200

    lydfiler virker

--- src/main/project/classes/AudioController.java
+++ src/main/project/classes/AudioController.java
@@ -2,17 +2,21 @@ package main.project.classes;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import java.net.URL;
+
 public class AudioController {
     private static MediaPlayer mediaPlayer;
 
     public static void playAudio(Audio audio) {
 
         String audioFilePath = audio.getFilePath();
-
-        Media media = new Media(audioFilePath);
-
+        URL resource = AudioController.class.getClassLoader().getResource(audioFilePath);
+        if (resource == null) {
+            System.out.println("Cannot find resource: " + audioFilePath);
+            return;
+        }
+        Media media = new Media(resource.toString());
         mediaPlayer = new MediaPlayer(media);
-
         mediaPlayer.play();
 
     }
