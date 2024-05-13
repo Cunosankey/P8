c5030609d8bab05bf65829c22b8c5db6d2f41310
Eberpraw <toby@easy.poker>
2024-05-13 23:57:14 +0200

    Updated task text

--- src/main/project/classes/TaskDescription.java
+++ src/main/project/classes/TaskDescription.java
@@ -24,7 +24,7 @@ public class TaskDescription {
     public static TaskDescription createTaskDescription1() {
         TaskDescription taskDescription1 = new TaskDescription();
         taskDescription1.setTitle("Hvordan har\nJeppe det?");
-        taskDescription1.setDescription("Hvordan tror du, at Jeppe har det efter hans ven overtog spillet for ham?\n\nEns følelser kan skinne igennem udefra, så nu skal du prøve at vise hvordan Jeppe har det ved at vælge hans ansigtsudtryk og kropsudtryk.");
+        taskDescription1.setDescription("Hvordan tror du, at Jeppe har det efter hans ven overtog spillet for ham?\n\nEns følelser kan skinne igennem udefra, så nu skal du prøve at vise hvordan Jeppe har det ved at vælge hans ansigtsudtryk og kropsudtryk.\n\nHusk, der er ingen rigtige eller forkerte svar.");
         return taskDescription1;
     }
 
